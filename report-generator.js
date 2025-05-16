d5bdfa7c44c5838999c1d1f83742e1c87d4dbdb1
Jenkins CI <riaz11@hotmail.com>
2025-05-15 21:12:09 -0400

    added d.ts file for html-reporter

--- report-generator.js
+++ report-generator.js
@@ -1,16 +1,28 @@
-import { generate } from 'cucumber-html-reporter';
+import { generate } from 'multiple-cucumber-html-reporter';
 
-const options = {
-  theme: 'bootstrap',
-  jsonFile: 'reports/json/cucumber_report.json',
-  output: 'reports/html/cucumber_report.html',
-  reportSuiteAsScenarios: true,
-  launchReport: false,
-  metadata: {
-    "Browser": "Chrome",
-    "Platform": "Mac",
-    "Executed": "Jenkins"
+generate({
+  jsonDir: 'reports/json',
+  reportPath: 'reports/html',
+  metadata:{
+    browser: {
+      name: 'chrome',
+      version: '120'
+    },
+    device: 'Jenkins Node',
+    platform: {
+      name: 'macOS',
+      version: 'Ventura'
+    }
+  },
+  customData: {
+    title: 'Run Info',
+    data: [
+      { label: 'Project', value: 'SEP Automation' },
+      { label: 'Release', value: '1.0.0' },
+      { label: 'Cycle', value: 'Regression' },
+      { label: 'Execution Start Time', value: new Date().toLocaleString() },
+      { label: 'Execution End Time', value: new Date().toLocaleString() }
+    ]
   }
-};
+});
 
-generate(options);
