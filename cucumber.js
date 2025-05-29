65839825045f1eec64d6519f591b72ba529a42c2
Jenkins CI <riaz11@hotmail.com>
2025-05-29 15:48:24 -0400

    Added Webhook

--- cucumber.js
+++ cucumber.js
@@ -5,7 +5,7 @@ module.exports = {
   default: {
 
     // Run tests in parallel using 2 workers
-    parallel: 6,
+    parallel: 2,
 
     // Location of feature files
     paths: ["./features/**/*.feature"],
